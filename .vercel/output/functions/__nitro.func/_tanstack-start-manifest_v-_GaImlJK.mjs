c940c6c4698e1a849e1f1b242f2a8c276b28ecaa
MD Motakabbir Morshed <dolardx@gmail.com>
2026-05-27 17:34:46 +0600

    feat: initialize Vercel deployment configuration and SSR production output

---
+++ .vercel/output/functions/__nitro.func/_tanstack-start-manifest_v-_GaImlJK.mjs
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "/media/dolar/office/projects/outline-essence-glow/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-D3aMQmmO.js"] }, "/": { filePath: "/media/dolar/office/projects/outline-essence-glow/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-CAUw32CT.js"] } }, clientEntry: "/assets/index-D3aMQmmO.js" });
+export {
+  tsrStartManifest
+};
