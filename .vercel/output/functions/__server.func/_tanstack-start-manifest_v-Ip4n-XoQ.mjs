89d73ba6fcc9aa2ac39c1b24e4a2bbd39be8fc37
MD Motakabbir Morshed <dolardx@gmail.com>
2026-05-27 18:04:49 +0600

    refactor: update build artifacts, add static assets, and refine CSS styling

---
+++ .vercel/output/functions/__server.func/_tanstack-start-manifest_v-Ip4n-XoQ.mjs
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "/media/dolar/office/projects/outline-essence-glow/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-CUDLH959.js"] }, "/": { filePath: "/media/dolar/office/projects/outline-essence-glow/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-DI4zMBYi.js"] } }, clientEntry: "/assets/index-CUDLH959.js" });
+export {
+  tsrStartManifest
+};
